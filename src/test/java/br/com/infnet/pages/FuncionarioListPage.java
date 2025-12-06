12a69d09510029c550d74415893e61947a988322
Nicolas Mendez Martins <mendezz.nick@gmail.com>
2025-12-06 09:28:51 -0300

    testando

--- src/test/java/br/com/infnet/pages/FuncionarioListPage.java
+++ src/test/java/br/com/infnet/pages/FuncionarioListPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-
+/* testando workflow */
 public class FuncionarioListPage {
 
     private final WebDriver driver;
